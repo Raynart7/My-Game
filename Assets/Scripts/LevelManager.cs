48fbe553dfcda6d6283e0437a8cf0d05e59db4aa
Emma Gillespie <emma7gillespie@gmail.com>
2016-04-18 19:34:55 -0600

    My first commit.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentCheckPoint;
+	private PlayerController player;
+
+	//public GameObject deathParticle;
+	public float respawnDelay;
+	//public GameObject respawnParticle;
+	//public int pointPenaltyOnDeath;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<PlayerController> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void RespawnPlayer(){
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//Instantiate (deathParticle, player.transform.position, player.transform.rotation);
+		player.enabled = false;
+		player.GetComponent<Renderer> ().enabled = false;
+
+		//ScoreManager.AddPoints (-pointPenaltyOnDeath);
+
+		Debug.Log ("Player Respawn");
+		yield return new WaitForSeconds (respawnDelay);
+		player.transform.position = currentCheckPoint.transform.position;
+		player.enabled = true;
+		player.GetComponent<Renderer> ().enabled = true;
+		//Instantiate (respawnParticle, currentCheckPoint.transform.position, currentCheckPoint.transform.rotation);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	//Player movement
+	public float moveSpeed;
+	public float jumpHeight;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//move right
+		if (Input.GetKey(KeyCode.D)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		//move left
+		if (Input.GetKey(KeyCode.A)) {
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+
+		//jump
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+		}
+						
+	
+	}
+}
